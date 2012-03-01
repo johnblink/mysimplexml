eb115df2eb0ca81b069c93da0df9a877d7858c18
johnblink <johndu12@hotmail.fr>
2012-03-01 16:52:54 +0100

    first commit

---
+++ MySimpleXML/src/main/java/dcll/jbon/mysimplexml/App.java
@@ -0,0 +1,13 @@
+package dcll.jbon.mysimplexml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
